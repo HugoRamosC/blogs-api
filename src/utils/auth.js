ec9f4c54cdad3196ee12c9ad9896da36cb77f8bd
HugoRamosC <hugor.castilho@gmail.com>
2023-03-03 18:30:21 -0300

    feat: jwt auth file - req 03 parcial

---
+++ src/utils/auth.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+const statusHTTP = require('./statusHTTPCodes');
+
+const secret = process.env.JWT_SECRET;
+
+const jwtConfig = {
+  expiresIn: '30d',
+  algorithm: 'HS256',
+};
+
+const generateToken = ({ id, email, password }) => {
+  if (!email || !password) {
+    const error = {
+      message: 'Some required fields are missing',
+    };
+    error.status = statusHTTP.BAD_REQUEST;
+    throw error;
+  }
+  jwt.sign({ id, email }, secret, jwtConfig);
+};
+
+const verifyToken = async (token) => {
+  if (!token) {
+    const error = new Error('missing token');
+    error.status = statusHTTP.ANAUTHORIZED;
+    throw error;
+  }
+
+  try {
+    const decryptedData = await jwt.verify(token, secret);
+    return {token:decryptedData};
+  } catch (e) {
+    const error = {
+      message: 'Invalid fields',
+    };
+    error.status = statusHTTP.BAD_REQUEST,
+  }
+
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
